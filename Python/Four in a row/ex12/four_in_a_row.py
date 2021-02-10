db24b777b4d953111993f75bdfcddec476af21b1
Eli Yacobov <eli.yacobov1@gmail.com>
2021-02-10 15:53:20 +0200

    initial commit

--- Python/Four in a row/ex12/four_in_a_row.py
+++
@@ -1,7 +0,0 @@
-import ex12.mygui as mygui
-import tkinter as tk
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    mygui.main_menu(root)
-    tk.mainloop()
--- Python/Four in a row/four_in_a_row.py
+++
@@ -1,7 +0,0 @@
-from ex12 import mygui
-import tkinter as tk
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    mygui.main_menu(root)
-    tk.mainloop()
